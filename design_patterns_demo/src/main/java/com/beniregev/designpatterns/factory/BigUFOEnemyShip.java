240fa28df2bcf66b7478af5c1d50536e843742fa
Binyamin Regev <beniregev@gmail.com>
2019-01-26 17:56:57 +0200

    Initial Version

---
+++ design_patterns_demo/src/main/java/com/beniregev/designpatterns/factory/BigUFOEnemyShip.java
@@ -0,0 +1,13 @@
+package com.beniregev.designpatterns.factory;
+
+public class BigUFOEnemyShip extends UFOEnemyShip {
+
+    public BigUFOEnemyShip(){
+
+        setName("Big UFO Enemy Ship");
+
+        setDamage(40.0);
+
+    }
+
+}
