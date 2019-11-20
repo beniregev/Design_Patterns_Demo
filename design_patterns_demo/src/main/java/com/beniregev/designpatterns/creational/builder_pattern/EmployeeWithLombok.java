cb4c83022bc2917dbfa4ce6a37610ecb8018a075
Binyamin Regev <binyamin.regev@nice.com>
2019-11-20 15:57:25 +0200

    Finished Builder Pattern demo with and without Lombok project

---
+++ design_patterns_demo/src/main/java/com/beniregev/designpatterns/creational/builder_pattern/EmployeeWithLombok.java
@@ -0,0 +1,31 @@
+package com.beniregev.designpatterns.creational.builder_pattern;
+
+
+import lombok.*;
+import lombok.experimental.Wither;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+@Data
+@Builder
+@Wither
+public class EmployeeWithLombok {
+    @NonNull
+    private String id;          //  Required
+    @NonNull
+    private String firstName;   //  Required
+    @NonNull
+    private String lastName;    //  Required
+    @NonNull
+    private Double salary;      //  Required
+    @NonNull
+    private String gender;      //  Required
+    private String address;     //  Optional
+    private String email;       //  Optional
+    private String phone;       //  Optional
+}
